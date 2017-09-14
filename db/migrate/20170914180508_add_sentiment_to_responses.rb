830b3e7c37b4938a23e1cb30925e94cb04d98483
Matthew Sullivan <matthew@msull92.com>
2017-09-14 18:35:47 -0500

    Fix scope issue

--- db/migrate/20170914180508_add_sentiment_to_responses.rb
+++ db/migrate/20170914180508_add_sentiment_to_responses.rb
@@ -3,11 +3,11 @@ require "google/cloud/language"
 class Response < ActiveRecord::Base
 end
 
-language_client = Google::Cloud::Language.new(project: ENV["GOOGLE_CLOUD_PROJECT_ID"])
-
 class AddSentimentToResponses < ActiveRecord::Migration[5.0]
   def change
     add_column :responses, :sentiment, :decimal
+
+    language_client = Google::Cloud::Language.new(project: ENV["GOOGLE_CLOUD_PROJECT_ID"])
 
     Response.all.each do |response|
       sentiment = language_client.document(response.body).sentiment.score
